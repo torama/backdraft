5418f09c034d51ece1e3a44b125f67a99fe87b7b
torama <torben.amann@googlemail.com>
2014-04-10 16:09:10 +0200

    Greeting Controller

---
+++ main/backdraft/Greeting.java
@@ -0,0 +1,19 @@
+package backdraft;
+
+public class Greeting {
+	private final long id;
+	private final String content;
+
+	public long getId() {
+		return id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public Greeting(long id, String content) {
+		this.id = id;
+		this.content = content;
+	}
+}
